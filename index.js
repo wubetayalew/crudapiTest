d89c86a1409e1179f6ee3fbe7a36f3a55e7acc2f
wubetayalew <wubetayalewcourse@gmail.com>
2023-08-04 01:35:37 +0300

    making the default display 'wel'

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const db = require("./config/db");
 const app = express();
 const mainRouter = require("./routes/main_route");
 const bodyParser = require("body-parser");
-app.use("/", mainRouter);
+// app.use("/", mainRouter);
+app.get("/", (req, res) => {
+  res.send("wel");
+});
 
 app.listen(9000, function () {
   console.log("server started");
