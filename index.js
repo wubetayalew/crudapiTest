7963f8049489337190ceab98fee539cac4b217d9
wubetayalew <wubetayalewcourse@gmail.com>
2023-08-04 01:43:32 +0300

    change made2

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
-const db = require("./config/db");
+// const db = require("./config/db");
 const app = express();
 const mainRouter = require("./routes/main_route");
 const bodyParser = require("body-parser");
 // app.use("/", mainRouter);
-app.get("/", (req, res) => {
-  res.send("wel");
-});
+app.get("/", mainRouter);
 
-app.listen(9000, function () {
-  console.log("server started");
+const connection = require("./config/db");
+app.listen(9000, async () => {
+  try {
+    await connection;
+    console.log("connection to db cloud");
+  } catch (err) {
+    console.log(err);
+  }
+  console.log("working on 9000");
 });
