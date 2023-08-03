b188450e96c7bcb303f17aa78deb73d65f5411c2
wubetayalew <wubetayalewcourse@gmail.com>
2023-08-04 01:27:32 +0300

    change made

--- index.js
+++ index.js
@@ -5,23 +5,8 @@ const db = require("./config/db");
 const app = express();
 const mainRouter = require("./routes/main_route");
 const bodyParser = require("body-parser");
-// app.use("/", mainRouter);
+app.use("/", mainRouter);
 
-// app.listen(9000, function () {
-//   console.log("server started");
-// });
-
-(async () => {
-  try {
-    await mongoose.connect(process.env.DB_CONNECTION_STRING);
-    const connection = mongoose.connection;
-    console.log("Connected to the database...");
-    app.use("/", mainRouter);
-    app.listen(9000, function () {
-      console.log("Server started");
-    });
-    module.exports = connection;
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-  }
-})();
+app.listen(9000, function () {
+  console.log("server started");
+});
