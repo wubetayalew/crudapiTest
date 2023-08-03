aa54423fbf809954da6a4afff64e41bbd2873cfd
wubetayalew <wubetayalewcourse@gmail.com>
2023-08-04 00:57:33 +0300

    server is served after database is connected

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const url = "mongodb://127.0.0.1:27017/crudapiDB";
 const mongodbAtlasUrl2 = process.env.DB_CONNECTION_STRING;
